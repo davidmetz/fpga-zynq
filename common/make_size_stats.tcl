068fa5e9e79ab51431463a800cfc148ba8d0d62d
David Metz <david.metz14@googlemail.com>
2020-05-19 02:17:30 +0200

    dump before new boom experiments

--- common/make_size_stats.tcl
+++ common/make_size_stats.tcl
@@ -4,4 +4,5 @@ launch_runs synth_size
 wait_on_run synth_size
 open_run synth_size -name synth_size
 report_utilization -cells [get_cells top/target/boom_tile] -hierarchical -file BOARD_NAME_HERE_rocketchip_CHISEL_CONFIG_HERE/BOARD_NAME_HERE_rocketchip_CHISEL_CONFIG_HERE_size.txt
+report_power -hier all -file BOARD_NAME_HERE_rocketchip_CHISEL_CONFIG_HERE/BOARD_NAME_HERE_rocketchip_CHISEL_CONFIG_HERE_power.txt
 exit
